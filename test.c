918f06f5e3807a62ab0aad8da477daa630267035
Milan Broz <gmazyland@gmail.com>
2019-03-11 14:28:50 +0100

    Add a simple callback to show benchmark progress.

--- test.c
+++ test.c
@@ -2,6 +2,13 @@
 #include <sys/mman.h>
 #include "crypto_backend.h"
 
+static int benchmark_callback(uint32_t time_ms, void *usrptr)
+{
+	printf("*");
+	fflush(stdout);
+	return 0;
+}
+
 int main (int argc, char *argv[])
 {
 	uint32_t iter, mem;
@@ -19,10 +26,10 @@ int main (int argc, char *argv[])
 
 	r = crypt_pbkdf_perf("argon2i", NULL, "test", 4,
 		"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f", 16,
-		32, 10000, 1024 *1024, 4, &iter, &mem, NULL, NULL);
+		32, 10000, 1024 *1024, 4, &iter, &mem, &benchmark_callback, NULL);
 
 	if (!r)
-		printf("Benchmark %u iterations, %u memory.\n", iter, mem);
+		printf("\nBenchmark %u iterations, %u memory.\n", iter, mem);
 
 	if (munlockall() == -1)
 		printf("Cannot unlock memory with munlockall.\n");
